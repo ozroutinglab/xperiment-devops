d02b2bd53f5e190adbd2d999b741c78761c597e6
ozlab <ozrouting.lab@gmail.com>
2026-02-28 20:21:44 +0000

    Move NPM registry and add new Docker engine installer in Master directory
    * Move NPM registry folder into Master directory
    + Add Docker engine folder into Master directory

---
+++ Master/DockerEngine/docker_clean_uninstall.sh
@@ -0,0 +1,9 @@
+#######################################################
+# clean_uninstall.sh
+#######################################################
+
+sudo apt remove $(dpkg --get-selections docker.io docker-compose docker-compose-v2 docker-doc podman-docker containerd runc | cut -f1)
+
+#######################################################
+# END #
+#######################################################
